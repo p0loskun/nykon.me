9e09c3605f02ccabf79afee6b3c5225cdf19f774
Nykon Hrytsyshyn <admin@nykon.me>
2025-01-02 16:12:57 +0300

    Fix mobile view

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ScrollShadow } from "@nextui-org/scroll-shadow";
 
 import { Head } from "@/src/layouts/head";
@@ -11,11 +11,22 @@ export default function DefaultLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const [isMobileView, setIsMobileView] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth < 768);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+  });
+
   return (
     <TransitionProvider>
       <Head />
       <TransitionScreen />
-      <Navbar />
+      <Navbar iconOnly={isMobileView} />
       <ScrollShadow className="w-full h-screen" hideScrollBar={true} size={100}>
         <main className="container mx-auto max-w-7xl px-6 flex-grow md:pt-16">
           {children}
