30b14b116dba5a3ad9f5af46fb10baa362027c7e
Nykon Hrytsyshyn <admin@nykon.me>
2025-01-02 17:16:31 +0300

    Fix shadow and scroll

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { ScrollShadow } from "@nextui-org/scroll-shadow";
 
 import { Head } from "@/src/layouts/head";
 import { Navbar } from "@/src/components/navbar";
@@ -27,11 +26,9 @@ export default function DefaultLayout({
       <Head />
       <TransitionScreen />
       <Navbar iconOnly={isMobileView} />
-      <ScrollShadow className="w-full h-screen" hideScrollBar={true} size={100}>
-        <main className="container mx-auto max-w-7xl px-6 flex-grow md:pt-16 max-md:pb-20">
-          {children}
-        </main>
-      </ScrollShadow>
+      <main className="container mx-auto max-w-7xl px-6 flex-grow md:pt-16 max-md:pb-20">
+        {children}
+      </main>
     </TransitionProvider>
   );
 }
